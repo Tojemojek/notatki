d9dec166ef16d95041b85609e2aeb9e851011701
Tojemojek <inz@kostrowski.pl>
2017-08-22 20:19:07 +0200

    "czyszczenie zbędnych danych"

--- 20170820Generyki/src/pl/sda/javawwa/Generic/Namable.java
+++
@@ -1,6 +0,0 @@
-package pl.sda.javawwa.Generic;
-
-public interface Namable {
-    public String getName();
-
-}
--- 20170820IOExceptionsSerializable/src/pl/sda/javawwa/myError/MojWyjatek.java
+++
@@ -1,9 +0,0 @@
-package pl.sda.javawwa.myError;
-
-public class MojWyjatek extends Exception {
-
-   public MojWyjatek(String message, Throwable cause) {
-       super(message, cause);
-   }
-
-}

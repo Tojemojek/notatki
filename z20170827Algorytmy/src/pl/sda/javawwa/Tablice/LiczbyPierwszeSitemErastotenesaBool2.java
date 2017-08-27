4efe933bdabbe1de47abc8f34911b5821daabacc
Tojemojek <inz@kostrowski.pl>
2017-08-27 13:57:04 +0200

    Sito Erastotenesa na liczby pierwsze wariant z tablicą boolowską (tym razem na false)

---
+++ z20170827Algorytmy/src/pl/sda/javawwa/Tablice/LiczbyPierwszeSitemErastotenesaBool2.java
@@ -0,0 +1,43 @@
+package pl.sda.javawwa.Tablice;
+
+import java.util.Scanner;
+
+public class LiczbyPierwszeSitemErastotenesaBool2 {
+    public static void main(String[] args) {
+
+        Integer rozmiarTablicy;
+        Integer j;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj do jakiej wartości chcesz sporawdzać:");
+        String userChoice = scanner.nextLine();
+
+        try {
+            rozmiarTablicy = Integer.parseInt(userChoice);
+        } catch (NumberFormatException e) {
+            System.out.println("Wprowadzono nieprawidłowe dane");
+            System.out.println(String.format("Twój wybór to %s a powinna być liczba całkowita", userChoice));
+            return;
+        }
+
+        boolean[] dane = new boolean[rozmiarTablicy + 1];
+
+        for (int i = 2; i < rozmiarTablicy + 1; i++) {
+            if (!dane[i]) {
+                j = 2 * i;
+                while (j < rozmiarTablicy + 1) {
+                    dane[j] = true;
+                    j += i;
+                }
+            }
+        }
+
+        for (int i = 2; i < rozmiarTablicy + 1; i++) {
+            if (!dane[i])
+                System.out.print(i + ", ");
+        }
+
+    }
+}
+
+
+

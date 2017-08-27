dca71599ca153b27c3bc460e850f24136a83e381
Tojemojek <inz@kostrowski.pl>
2017-08-27 13:55:08 +0200

    Sito Erastotenesa na liczby pierwsze wariant z tablicą boolowską

--- z20170827Algorytmy/src/pl/sda/javawwa/Tablice/LiczbyPierwszeSitemErastotenesa.java
+++ z20170827Algorytmy/src/pl/sda/javawwa/Tablice/LiczbyPierwszeSitemErastotenesa.java
@@ -1,20 +1,36 @@
 package pl.sda.javawwa.Tablice;
 
+import java.util.Scanner;
+
 public class LiczbyPierwszeSitemErastotenesa {
     public static void main(String[] args) {
 
-        Integer rozmiarTablicy = 500;
+        Integer rozmiarTablicy;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj do jakiej wartości chcesz sporawdzać:");
+        String userChoice = scanner.nextLine();
+
+        try {
+            rozmiarTablicy = Integer.parseInt(userChoice);
+        } catch (NumberFormatException e) {
+            System.out.println("Wprowadzono nieprawidłowe dane");
+            System.out.println(String.format("Twój wybór to %s a powinna być liczba całkowita", userChoice));
+            return;
+        }
+
         Integer[] dane = new Integer[rozmiarTablicy];
         int j;
-        int tmp;
 
         System.out.println("Przypisanie danych do tablicy");
         for (int i = 0; i < rozmiarTablicy; i++) {
             dane[i] = i + 1;
         }
+
         for (Integer tmp1 : dane) {
             System.out.print(tmp1 + ", ");
         }
+
         System.out.println("\nEfekt sita");
         for (int i = 1; i < rozmiarTablicy; i++) {
             if (!dane[i].equals(0)) {
